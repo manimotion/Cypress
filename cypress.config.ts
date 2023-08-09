0ffadc41d6a6329a63c451f648caa68354111fba
Jose Orjuela <jose.orjuela@perficient.com>
2023-08-09 08:27:01 -0500

    Setup CI

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,15 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    // solo necesitas agregar estas dos propiedades, no incluir este comentario
+    video: false,
+    screenshotOnRunFailure: false,
+    chromeWebSecurity: false,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    // e2e testing node events setup code
+    config.defaultCommandTimeout= 10000;
+    config.responseTimeout= 20000;
+    config.pageLoadTimeout= 60000;
+    return config;
   },
 });
