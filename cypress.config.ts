69ff701898efbbb24ce9fd1203f45166d77a8ed0
manimotion <69907153+manimotion@users.noreply.github.com>
2023-08-17 11:47:41 -0500

    setup CI (#7)
    * Adding .eslintrc.js to the root
    
    * Setup CI
    ---------
    
    Co-authored-by: Jose Orjuela <jose.orjuela@perficient.com>

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,16 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    video: false,
+    screenshotOnRunFailure: false,
+    chromeWebSecurity: false,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+    // e2e testing node events setup code
+      config.defaultCommandTimeout= 10000;
+      config.responseTimeout= 20000;
+      config.pageLoadTimeout= 60000;
+      return config;
     },
   },
-});
+}
+);
